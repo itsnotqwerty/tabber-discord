a8d279e4125215adac3c8a49d93df9ffc71d8865
Samuel Roux <samroux808@gmail.com>
2026-03-23 10:57:08 -0500

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,50 @@
+import discord
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!lookup"):
+        target = message.content.split(" ")[1:]
+        if len(target) < 1:
+            await message.channel.send("Usage: !lookup <PERSON_NAME>")
+            return
+        target = " ".join(target)
+        api_url = "https://tabber.cicis.info/lookup"
+        response = requests.post(
+            api_url,
+            json={"name": target},
+            headers={"Content-Type": "application/json", "Host": "tabber.cicis.info"},
+            verify=False,
+        )
+        if response.status_code == 200:
+            data = response.json()
+            result = data["result"]
+            if result:
+                await message.channel.send(
+                    f"**Name:** {data['canon_name']}\n**Location (Best Guess):** {result['location']}\n**Reasoning:** {result['reasoning']}"
+                )
+            else:
+                await message.channel.send("No results found.")
+        else:
+            await message.channel.send("Lookup failed. Please try again later.")
+
+
+client.run(os.getenv("DISCORD_TOKEN"))
